84123be943982989a9af1401efec0306f389610e
cicero884 <joy861225@gmail.com>
2021-10-24 15:59:19 +0800

    set ui and finish prob1-1

--- hw1/main.py
+++ hw1/main.py
@@ -1,25 +1,42 @@
-from PyQt5.QtWidgets import *
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
 import sys
+import prob1
 
 
-app = QApplication([])
-window = QWidget()
-layout = QVBoxLayout()
+class Ui_MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Ui_MainWindow, self).__init__(None)
+        uic.loadUi("mainwindow.ui", self)
+		
+        #prob1
+        self.btn1_1.clicked.connect(prob1.prob1_1)
+        #self.btn4_1.clicked.connect(self.job4_1)
 
-#prob1
-prob1_group=QGroupBox("1.Camera Calibration", window)
-layout.addWidget(prob1_group)
-prob1_layout=QVBoxLayout()
-prob1_group.setLayout(prob1_layout)
 
-prob1_layout.addWidget(QPushButton('Corner detection'))
-prob1_layout.addWidget(QPushButton('Find the intrinsic matrix'))
-prob1_layout.addWidget(QPushButton('Find the extrinsic matrix'))
-prob1_layout.addWidget(QPushButton('Find the distortion matrix'))
-prob1_layout.addWidget(QPushButton('Show the undistorted result'))
 
-#prob2
+if __name__ == "__main__": 
+    Application = QtWidgets.QApplication(sys.argv)
+    mainwindow=Ui_MainWindow()
+    mainwindow.show()
+    sys.exit(Application.exec_())
 
-window.setLayout(layout)
-window.show()
-app.exec()
+#
+##prob1
+#prob1_group=QGroupBox("1.Camera Calibration", window)
+#layout.addWidget(prob1_group)
+#prob1_layout=QVBoxLayout()
+#prob1_group.setLayout(prob1_layout)
+#
+#prob1_layout.addWidget(QPushButton('Corner detection'))
+#prob1_1_btn=QPushButton('intrinsic matrix')
+#prob1_1_btn.clicked.connect(prob1_1)
+#prob1_layout.addWidget(prob1_1_btn)
+#prob1_layout.addWidget(QPushButton('extrinsic matrix'))
+#prob1_layout.addWidget(QPushButton('distortion matrix'))
+#prob1_layout.addWidget(QPushButton('undistorted result'))
+#
+##prob2
+#
+#window.setLayout(layout)
+#window.show()
+#app.exec()
--- hw1/prob1.py
+++ hw1/prob1.py
@@ -1,1 +1,16 @@
-import 
+import cv2
+import numpy as np
+
+CHESS_SIZE=(11,8)
+
+def prob1_1(self):
+    print("here")
+    for i in range(1, 15):
+        img=cv2.imread(f"Dataset_CvDl_Hw1/Q1_Image/{i}.bmp")
+        ret,corners=cv2.findChessboardCorners(img,CHESS_SIZE)
+        if ret==True:
+            #cv2.cornerSubPix
+            cv2.drawChessboardCorners(img,CHESS_SIZE,corners,ret)
+            cv2.imshow('12x9 chessborad', img)
+            cv2.waitKey(500)
+    cv2.destroyAllWindows()
