1b3c63214b43bb9e370c3b50ec2bea1472bd5fa9
cicero884 <joy861225@gmail.com>
2021-10-26 21:52:29 +0800

    Fix prob1 camera calibration

--- hw1/prob1.py
+++ hw1/prob1.py
@@ -4,11 +4,12 @@ import numpy as np
 CHESS_SIZE=(11,8)
 
 def prob1_1():
-    for i in range(1, 15):
+    for i in range(1, 16):
         img=cv2.imread(f"Dataset_CvDl_Hw1/Q1_Image/{i}.bmp")
         ret,corners=cv2.findChessboardCorners(img,CHESS_SIZE)
         if ret==True:
             cv2.drawChessboardCorners(img,CHESS_SIZE,corners,ret)
+            img=cv2.resize(img,(720,720))
             cv2.imshow('12x9 chessborad', img)
             cv2.waitKey(500)
     cv2.destroyAllWindows()
@@ -20,7 +21,7 @@ def prob1_2():
 
     objpoints = [] # 3d point in real world space
     imgpoints = [] # 2d points in image plane.
-    for i in range(1, 15):
+    for i in range(1, 16):
         img=cv2.imread(f"Dataset_CvDl_Hw1/Q1_Image/{i}.bmp")
         ret,corners=cv2.findChessboardCorners(img,CHESS_SIZE,None)
         if ret==True:
@@ -35,12 +36,19 @@ def prob1_2():
 def prob1_3(img_index):
     objp = np.zeros((11*8,3), np.float32)
     objp[:,:2] = np.mgrid[0:11,0:8].T.reshape(-1,2)
-    img=cv2.imread(f"Dataset_CvDl_Hw1/Q1_Image/{img_index}.bmp")
-    ret,corners=cv2.findChessboardCorners(img,CHESS_SIZE,None)
-    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera([objp], [corners], img.shape[::-2], None, None)
-    rvecs = cv2.Rodrigues(rvecs[0])
+
+    objpoints = [] # 3d point in real world space
+    imgpoints = [] # 2d points in image plane.
+    for i in range(1, 16):
+        img=cv2.imread(f"Dataset_CvDl_Hw1/Q1_Image/{i}.bmp")
+        ret,corners=cv2.findChessboardCorners(img,CHESS_SIZE,None)
+        if ret==True:
+            objpoints.append(objp)
+            imgpoints.append(corners)
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img.shape[::-2], None, None)
+    rvecs = cv2.Rodrigues(rvecs[img_index-1])
     print("Extrinsic:")
-    print(np.append(rvecs[0],tvecs[0],axis=1))
+    print(np.append(rvecs[0],tvecs[img_index-1],axis=1))
 
 def prob1_4():
     # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(10,7,0)
@@ -49,7 +57,7 @@ def prob1_4():
 
     objpoints = [] # 3d point in real world space
     imgpoints = [] # 2d points in image plane.
-    for i in range(1, 15):
+    for i in range(1, 16):
         img=cv2.imread(f"Dataset_CvDl_Hw1/Q1_Image/{i}.bmp")
         ret,corners=cv2.findChessboardCorners(img,CHESS_SIZE,None)
         if ret==True:
@@ -58,26 +66,32 @@ def prob1_4():
 
     ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img.shape[::-2], None, None)
     print("Distortion:")
-    print(dist[0])
+    print(dist)
 
 def prob1_5():
+    img=[]
     objp = np.zeros((11*8,3), np.float32)
     objp[:,:2] = np.mgrid[0:11,0:8].T.reshape(-1,2)
 
-    #for i in range(1, 15):
-    i=12
-    img=cv2.imread(f"Dataset_CvDl_Hw1/Q1_Image/{i}.bmp")
-    ret,corners=cv2.findChessboardCorners(img,CHESS_SIZE)
-    if ret==True:
-        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera([objp], [corners], img.shape[::-2], None, None)
-        h,  w = img.shape[:2]
-        print(h,w)
+    objpoints = [] # 3d point in real world space
+    imgpoints = [] # 2d points in image plane.
+    for i in range(1, 16):
+        img.append(cv2.imread(f"Dataset_CvDl_Hw1/Q1_Image/{i}.bmp"))
+        ret,corners=cv2.findChessboardCorners(img[i-1],CHESS_SIZE)
+        if ret==True:
+            objpoints.append(objp)
+            imgpoints.append(corners)
+
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img[0].shape[::-2], None, None)
+    for i in range(1, 16):
+        h,  w = img[i-1].shape[:2]
         newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
         # undistort
-        dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+        dst = cv2.undistort(img[i-1], mtx, dist, None, newcameramtx)
+        dst=np.append(dst,img[i-1],axis=1)#dst = dst[y:y+h,x:x+w] 
+        dst=cv2.resize(dst,(1440,720))
         # crop the image
-        x, y, w, h = roi
-        dst = dst[y:y+h, x:x+w]
+        #x, y, w, h = roi
         cv2.imshow('12x9 chessborad', dst)
         cv2.waitKey(500)
     cv2.destroyAllWindows()
