0423018b2e9a9e7c9a38cddf25cd48a7e89e102f
cicero884 <joy861225@gmail.com>
2021-11-03 16:50:34 +0800

    add prob5

--- hw1/main_prob5.py
+++ hw1/main_prob5.py
@@ -1,7 +1,54 @@
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
 import sys
 
-import prob5
+import prob5_train
+
+cifar_label=['airplane','automobile','bird','cat','deer','dog','frog','horse','ship','trunk']
+
+def prob5_1():
+    fig = plt.gcf()
+    fig.set_size_inches(35, 35)
+    for i in range(9):
+        ax = plt.subplot(3, 3, 1 + i)
+        # ax.
+        ax.imshow(x_train[i])
+        ax.set_title(cifar_label[Y_train[i][0]])
+    plt.show()
+
+def prob5_2():
+    print('Batch Size {}',format(Batch_size))
+    model=prob5_train.load_model()
+    parameter=model.get_config()
+    print('\n{}',parameter)
+
+def prob5_3():
+    model=prob5_train.construct_vgg16()
+    print(model.summary())
+
+def prob5_4():
+    accuracy = img.imread('accuracy.png')
+    loss = img.imread('loss.png')
+    plt.imshow(accuracy)
+    plt.show()
+    plt.imshow(loss)
+    plt.show()
+
+def prob5_5(index):
+    model=prob5_train.load_model()
+    predict = model.predict(x_test[n].reshape(-1, 32, 32, 3))
+
+    testlebal = cifar_label[Y_test[n][0]]
+    plt.title(testlebal)
+    plt.imshow(x_test[n])
+    plt.show()
+
+    fig = plt.figure()
+    x = np.arange(len(cifar_label))
+    plt.bar(x, predict[0])
+    plt.xticks(x, cifar_label)
+    fig.set_figheight(6)
+    fig.set_figwidth(10)
+    plt.show()
 
 class Ui_MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -9,12 +56,12 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         uic.loadUi("mainwindow.ui", self)
 		
         #prob1
-        self.btn5_1.clicked.connect(prob5.prob5_1)
-        self.btn5_2.clicked.connect(prob5.prob5_2)
-        self.btn5_3.clicked.connect(prob5.prob5_3)
-        self.btn5_4.clicked.connect(prob5.prob5_4)
+        self.btn5_1.clicked.connect(prob5_1)
+        self.btn5_2.clicked.connect(prob5_2)
+        self.btn5_3.clicked.connect(prob5_3)
+        self.btn5_4.clicked.connect(prob5_4)
         self.btn5_5.clicked.connect(lambda: prob5.prob5_5(self.spinBox5_5.value()))
-        self.btn5_6.clicked.connect(prob5.prob5_6)
+        self.btn5_6.clicked.connect(prob5_train.train())
         
 
 if __name__ == "__main__": 
--- hw1/prob5_train.py
+++ hw1/prob5_train.py
@@ -7,6 +7,83 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D
 from tensorflow.keras.models import Sequential
 import numpy as np
 
+global Batch_size=128
+
+global (x_train, Y_train), (x_test, Y_test) = cifar10.load_data()
+x_train_normalized = x_train.astype('float32') / 255
+x_test_normalized = x_test.astype('float32') / 255
+
+y_train = to_categorical(Y_train)
+y_test = to_categorical(Y_test)
+
+def construct_vgg16():
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(32, 32, 3)))
+    model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+    model.add(Dropout(0.25))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+    model.add(Dropout(0.25))
+
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+    model.add(Dropout(0.25))
+
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+    model.add(Dropout(0.25))
+
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+    model.add(Dropout(0.25))
+
+    model.add(Dropout(0.25))
+
+    model.add(Dense(512, activation='relu'))
+    model.add(Dense(512, activation='relu'))
+    model.add(Dropout(0.25))
+    model.add(Dense(10, activation='softmax'))
+
+    return model
+
+def output_img(loss,acc):
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('Model accuracy')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Validation'], loc='upper left')
+    plt.savefig('accuracy.png', bbox_inches='tight')
+
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Model loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Validation'], loc='upper left')
+    plt.savefig('loss.png', bbox_inches='tight')
+
+def load_model():
+    model=load_model('q5_trained.h')
+    return model
+
 def train():
-    model=construct_vgg()
+    model=construct_vgg16()
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    history = model.fit(x=x_train_normalized,y=y_train,batch_size=Batch_size,epochs=50,validation_split=0.1,)
+    model.save('q5_trained.h')
+    # from keras.models import load_model
+    loss,acc=model.evaluate()
+    output_img(loss,acc)
+
+
+
+if __name__ == '__main__':
+    train()
